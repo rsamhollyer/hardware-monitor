c1978a8eb41ad4de8b08d5ff988afd3bd97b4520
Sam Hollyer <rsamhollyer@gmail.com>
2026-09-06 12:02:52 -0500

    add hardservice file

---
+++ src/main/java/com/example/hardwaremonitor/service/NativeHardwareService.java
@@ -0,0 +1,78 @@
+package com.example.hardwaremonitor.service;
+
+import com.sun.management.OperatingSystemMXBean;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class NativeHardwareService {
+
+
+    public Map<String, Object> getMetrics() {
+        Map<String, Object> metrics = new HashMap<>();
+
+        metrics.put("cpu", getCpuUsage());
+        metrics.put("memory", getMemoryUsage());
+        metrics.put("disk", getDiskUsage());
+
+        return metrics;
+    }
+
+    private @NonNull Map<String, Object> getCpuUsage() {
+        Map<String, Object> cpu = new HashMap<>();
+        OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+        cpu.put("systemCpuLoad", osBean.getCpuLoad() * 100);
+        cpu.put("processCpuLoad", osBean.getProcessCpuLoad() * 100);
+        cpu.put("availableProcessors", osBean.getAvailableProcessors());
+
+        return cpu;
+    }
+
+    private @NonNull Map<String, Object> getMemoryUsage() {
+        Map<String, Object> mem = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("/proc/meminfo"))) {
+            String line;
+            long memTotal = 0;
+            long memAvailable = 0;
+
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("MemTotal:")) {
+                    memTotal = lineReplace(line);
+                } else if (line.startsWith("MemAvailable:")) {
+                    memAvailable = lineReplace(line);
+                    break;
+                }
+            }
+            mem.put("totalKb", memTotal);
+            mem.put("availableKb", memAvailable);
+            mem.put("usedKb", memTotal - memAvailable);
+        } catch (Exception e) {
+            mem.put("error", "Failed to read:" + e.getMessage());
+        }
+
+        return mem;
+    }
+
+    private @NonNull Long lineReplace(@NonNull String line) {
+        return Long.parseLong(line.replaceAll("[^0-9]", ""));
+    }
+
+    private @NonNull Map<String, Object> getDiskUsage() {
+        Map<String, Object> disk = new HashMap<>();
+        File root = new File("/");
+
+        disk.put("totalBytes", root.getTotalSpace());
+        disk.put("freeBytes", root.getFreeSpace());
+        disk.put("usableBytes", root.getUsableSpace());
+
+        return disk;
+    }
+}
