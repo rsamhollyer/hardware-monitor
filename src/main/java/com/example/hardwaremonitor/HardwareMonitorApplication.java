9c03db94dae6770491d44bf2fc5a8790e954ec1f
Sam Hollyer <rsamhollyer@gmail.com>
2026-09-06 12:02:33 -0500

    add main file

---
+++ src/main/java/com/example/hardwaremonitor/HardwareMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.hardwaremonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareMonitorApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HardwareMonitorApplication.class, args);
+    }
+
+}
